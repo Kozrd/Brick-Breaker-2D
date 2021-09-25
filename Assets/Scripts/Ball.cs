003adc58d22dbbc6b6cae5f2bfd0e0fa5f7996fb
Niko Ziljian Revo Devara <nziljian@gmail.com>
2021-09-26 05:03:07 +0700

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+
+    public float speed;
+
+    private void Awake()
+    {
+        this.rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        ResetBall();
+    }
+
+    public void ResetBall()
+    {
+        this.transform.position = Vector2.zero;
+        this.rigidbody.velocity = Vector2.zero;
+
+        Invoke(nameof(SetRandomTrajectory), 1f);
+    }
+
+    private void SetRandomTrajectory()
+    {
+        Vector2 force = Vector2.zero;
+        force.x = Random.Range(-1f, 1);
+        force.y = -1f;
+
+        this.rigidbody.AddForce(force.normalized * speed);
+    }
+}
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball { get; private set; }
+
+    public Paddle paddle { get; private set; }
+
+    public Bricks[] bricks { get; private set; }
+
+    public int level = 1;
+
+    public int Score = 0;
+
+    Text ScoreUI;
+
+    public int lives = 3;
+
+    //GameObject PanelSelesai;
+    //Text txWin;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        SceneManager.sceneLoaded += OnLevelLoaded;
+    }
+
+    private void Start()
+    {
+        NewGame();
+        //PanelSelesai = GameObject.Find("PanelSelesai");
+       // PanelSelesai.SetActive(false);
+    }
+    
+    private void NewGame()
+    {
+        this.Score = 0;
+        this.lives = 3;
+        
+        LoadLevel(1);
+    }
+
+    private void LoadLevel(int level)
+    {
+        this.level = level;
+        if (level > 2)
+        {
+            SceneManager.LoadScene("You Win");
+        }
+        else
+        {
+            SceneManager.LoadScene("Level" + level);
+        }
+       
+    }
+
+    private void OnLevelLoaded(Scene scene, LoadSceneMode mode)
+    {
+        this.ball = FindObjectOfType<Ball>();
+        this.paddle = FindObjectOfType<Paddle>();
+        this.bricks = FindObjectsOfType<Bricks>();
+    }
+
+    public void ResetLevel()
+    {
+        this.ball.ResetBall();
+        this.paddle.ResetPaddle();
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+        this.lives = 3;
+        //NewGame();
+    }
+
+    public void Miss()
+    {
+        this.lives--;
+
+        if (this.lives > 0)
+        {
+            ResetLevel();
+        }
+        else
+        {
+            GameOver(); 
+        }
+    }
+
+    public void Hit(Bricks bricks)
+    {
+        this.Score += bricks.points;
+        ScoreUI = GameObject.Find("ScoreText").GetComponent<Text>();
+        Debug.Log("Score = " + Score);
+        ScoreUI.text = "Score = " + Score;
+        if (Cleared())
+        {
+            LoadLevel(this.level + 1);
+        }
+        //if (Score == 7800)
+        {
+            //PanelSelesai.SetActive(true);
+            //txWin = GameObject.Find("Win").GetComponent<Text>();
+            //txWin.text = "You Win";
+            //Destroy(gameObject);
+            //return;
+        }
+    }
+
+    private bool Cleared()
+    {
+        for (int i = 0; i < this.bricks.Length; i++)
+        {
+            if (this.bricks[i].gameObject.activeInHierarchy && !this.bricks[i].unbreakable)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
